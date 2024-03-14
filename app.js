c7210515cbe4db2f676b983fcc64f021ed14a051
UmarFarooq <153651315+UmerFaooq46@users.noreply.github.com>
2024-03-15 02:31:06 +0500

    ARRAY
    1. Declare an empty array using JS literal notation to store
    student names in future.
    2. Declare an empty array using JS object notation to store
    student names in future.
    3. Declare and initialize a strings array.
    4. Declare and initialize a numbers array.
    5. Declare and initialize a boolean array.
    6. Declare and initialize a mixed array.
    7. Declare and Initialize an array and store available
    education qualifications in Pakistan (e.g. SSC, HSC, BCS,
    BS, BCOM, MS, M. Phil., PhD). Show the listed
    qualifications in your browser like:

---
+++ app.js
@@ -0,0 +1,60 @@
+// ============QUESTION 01=====================================
+
+// var myArray = [];
+
+// myArray.push("Fahad");
+// myArray.push("Farooq");
+// myArray.push(19);
+
+// console.log(myArray[0]); 
+// console.log(myArray[1]); 
+// console.log(myArray[2]);
+
+// ============QUESTION 02=====================================
+
+// var studentNamesArray = {
+//     firstname: ["fahad"],
+//     last: ["umer"],
+//     Age: [19]
+// };
+// console.log(studentNamesArray); 
+
+// ============QUESTION 03=====================================
+
+// var stringsArray = ["Fahad", "Subhan", "Omer", "Areeb"];
+// console.log(stringsArray);
+
+// ============QUESTION 04=====================================
+// var intArray = ["19", "14", "17", "7"];
+// console.log(intArray);
+
+// ============QUESTION 05=====================================
+
+// let booleanArray = [true, false, true, true, false];
+// console.log(booleanArray);
+
+// ============QUESTION 06=====================================
+
+// let mixedArray = ['fahad', 19, true, { name: 'umer', age: 17 }, null];
+// console.log(mixedArray);
+
+
+// ============QUESTION 07=====================================
+
+// var names = [];
+// console.log(names);
+ 
+// var qualifications = [
+//     '01) SSC - Secondary School Certificate',
+//     '02) HSC - Higher Secondary Certificate',
+//     '03) BCS - Bachelor of Computer Science',
+//     '04) BS - Bachelor of Science',
+//     '05) BCOM - Bachelor of Commerce',
+//     '06) MS - Master of Science',
+//     '07) M. Phil. - Master of Philosophy',
+//     '08) PhD - Doctor of Philosophy'
+// ]; 
+
+// for (var i = 0; i < qualifications.length; i++) {
+//     document.writeln(qualifications[i] + "<br>");
+// }
